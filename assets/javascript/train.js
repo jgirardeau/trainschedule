337f0af762377f517a77936fae7a99c0e6d7abe2
Jim Girardeau <jwgirardeau@gmail.com>
2018-11-13 20:15:54 -0600

    working

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -21,24 +21,32 @@ function addTableItem(table, item) {
     table.append(td);
 }
 
-function get_time_delta(firstArrival) {
+function get_time_delta(firstArrival, frequencyMinutes) {
     console.log("First arrival ", firstArrival);
-    var a = moment(firstArrival, "HH:MM");
-    now = new moment();
-    diffDuration = moment.duration(a.diff(now));
-    console.log(diffDuration.seconds());
-    return 10;
-
+    var firstArrivalConverted = moment(firstArrival, "HH:mm").subtract(1, 'years');
+    console.log("first time converted " + firstArrivalConverted)
+        // diffDuration = now.duration(a.diff(now));
+    var diffTime = moment().diff(moment(firstArrivalConverted), "minutes");
+    console.log("diff time " + diffTime);
+    var modulo = diffTime % frequencyMinutes;
+    console.log("remainder " + modulo)
+    var tMinutesTillTrain = frequencyMinutes - modulo;
+    console.log("next train in min " + tMinutesTillTrain)
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    var nextTrainArrivalTime = moment(nextTrain).format("hh:mm")
+    console.log("ARRIVAL TIME: " + nextTrainArrivalTime);
+    return [tMinutesTillTrain, nextTrainArrivalTime];
 }
 
 function addTableEntry(table) {
-    var time_delta = get_time_delta(table.firstArrival);
+    var time_delta = get_time_delta(table.firstArrival, table.frequencyMinutes);
+    console.log("p1 ", time_delta[0], " p2 ", time_delta[1])
     var tr = $("<tr>");
     addTableItem(tr, table.trainName);
     addTableItem(tr, table.destination);
     addTableItem(tr, table.frequencyMinutes);
-    addTableItem(tr, time_delta);
-    addTableItem(tr, "0")
+    addTableItem(tr, time_delta[1]);
+    addTableItem(tr, time_delta[0]);
     $("#tableBody").append(tr);
 }
 
