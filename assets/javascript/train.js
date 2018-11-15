80786324487b716f7fc40cdea537a4eea0167dc4
Jim Girardeau <jwgirardeau@gmail.com>
2018-11-14 23:04:46 -0600

    update button

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -24,29 +24,32 @@ function addTableItem(table, item) {
 function get_time_delta(firstArrival, frequencyMinutes) {
     console.log("First arrival ", firstArrival);
     var firstArrivalConverted = moment(firstArrival, "HH:mm").subtract(1, 'years');
-    console.log("first time converted " + firstArrivalConverted)
-        // diffDuration = now.duration(a.diff(now));
+    // console.log("first time converted " + firstArrivalConverted)
+    // diffDuration = now.duration(a.diff(now));
     var diffTime = moment().diff(moment(firstArrivalConverted), "minutes");
-    console.log("diff time " + diffTime);
+    // console.log("diff time " + diffTime);
     var modulo = diffTime % frequencyMinutes;
-    console.log("remainder " + modulo)
+    // console.log("remainder " + modulo)
     var tMinutesTillTrain = frequencyMinutes - modulo;
-    console.log("next train in min " + tMinutesTillTrain)
+    // console.log("next train in min " + tMinutesTillTrain)
     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
     var nextTrainArrivalTime = moment(nextTrain).format("hh:mm")
-    console.log("ARRIVAL TIME: " + nextTrainArrivalTime);
+        // console.log("ARRIVAL TIME: " + nextTrainArrivalTime);
     return [tMinutesTillTrain, nextTrainArrivalTime];
 }
 
-function addTableEntry(table) {
+function addTableEntry(table, key) {
     var time_delta = get_time_delta(table.firstArrival, table.frequencyMinutes);
-    console.log("p1 ", time_delta[0], " p2 ", time_delta[1])
+    // console.log("p1 ", time_delta[0], " p2 ", time_delta[1])
     var tr = $("<tr>");
     addTableItem(tr, table.trainName);
     addTableItem(tr, table.destination);
     addTableItem(tr, table.frequencyMinutes);
     addTableItem(tr, time_delta[1]);
     addTableItem(tr, time_delta[0]);
+    tr.attr('id', key);
+    tr.attr('firstArrival', table.firstArrival);
+    tr.attr('frequencyMinutes', table.frequencyMinutes);
     $("#tableBody").append(tr);
 }
 
@@ -63,7 +66,7 @@ function validText(name, htmlRef) {
 
 function validIntegerPositive(name, htmlRef) {
     var validIntegerPositive = false;
-    console.log("Check Integer " + name);
+    // console.log("Check Integer " + name);
     var intVal = parseInt(name);
     if (Number.isInteger(intVal) && intVal > 0) {
         htmlRef.removeClass("errorShow").addClass("errorHide");
@@ -77,9 +80,9 @@ function validIntegerPositive(name, htmlRef) {
 
 function validMilitaryTime(name, htmlRef) {
     var validMilitaryTime = true;
-    console.log("Check military " + name);
+    // console.log("Check military " + name);
     var array = name.split(':');
-    console.log("Array ", array.length, " ", array, " ", array[0]);
+    // console.log("Array ", array.length, " ", array, " ", array[0]);
     if (array.length != 2) {
         validMilitaryTime = false;
         console.log("array length fails");
@@ -89,22 +92,22 @@ function validMilitaryTime(name, htmlRef) {
 
     } else {
         validMilitaryTime = false;
-        console.log("hours fail");
+        // console.log("hours fail");
     }
     intVal = parseInt(array[1]);
     if (Number.isInteger(intVal) && intVal >= 0 && intVal < 60) {
 
     } else {
         validMilitaryTime = false;
-        console.log("min fail");
+        // console.log("min fail");
     }
 
     if (!validMilitaryTime) {
-        console.log("HIDE MILITARY");
-        htmlRef.removeClass("errorShow").addClass("errorHide");
-    } else {
-        console.log("SHOW MILITARY");
+        // console.log("HIDE MILITARY");
         htmlRef.removeClass("errorHide").addClass("errorShow");
+    } else {
+        // console.log("SHOW MILITARY");
+        htmlRef.removeClass("errorShow").addClass("errorHide");
     }
 
     return validMilitaryTime;
@@ -125,8 +128,8 @@ $("#submitButton").click(function() {
     var firstArrival = $("#firstArrival").val();
     if (!validText(firstArrival, $("#firstArrivalError"))) errorInForm = true;
     if (!errorInForm) {
-        if (!validMilitaryTime(firstArrival, $("#firstArrivalMilitaryTimeError"))) {
-            console.log("military time error");
+        if (!validMilitaryTime(firstArrival, $("#firstArrivalTimeError"))) {
+            // console.log("military time error");
             errorInForm = true;
         }
     }
@@ -146,10 +149,30 @@ $("#submitButton").click(function() {
 //  database.ref().orderBychild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
 //   database.ref().on("child_added", function(snapshot) {
 database.ref().orderByChild("dateAdded").on("child_added", function(snapshot) {
+    console.log(snapshot)
     var table = snapshot.val();
+    var key = snapshot.key;
+    console.log(table);
     console.log(table.trainName);
     console.log(table.destination);
     console.log(table.firstArrival);
     console.log(table.frequencyMinutes);
-    addTableEntry(table);
+    addTableEntry(table, key);
+});
+
+$("#testButton").click(function() {
+    event.preventDefault();
+    var table = document.querySelector("#tableBody");
+    for (let row of table.rows) {
+        console.log(row.cells[3]);
+        console.log(row.cells[4]);
+        console.log(row.getAttribute('data'));
+        var firstArrival = row.getAttribute('firstArrival');
+        var frequencyMinutes = row.getAttribute('frequencyMinutes');
+        var time_delta = get_time_delta(firstArrival, frequencyMinutes);
+        console.log("p1 ", time_delta[0], " p2 ", time_delta[1])
+        row.cells[3].innerText = time_delta[1];
+        row.cells[4].innerText = time_delta[0];
+
+    }
 });
